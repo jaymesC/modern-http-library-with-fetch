d7b5eb41167234a6018c41d2614a3dc69fd5931e
James <clementjaymes@.com>
2021-08-14 16:08:32 -0700

    adding initial project files

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = new EasyHTTP
+
+// GET users
+// http.get('https://jsonplaceholder.typicode.com/users')
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
+
+//  User Data
+const data = {
+  name: 'Wesley Snipes',
+  username:'Wesley100',
+  email: 'snipes@gmail.com'
+}
+
+// Create user
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
+
+// create Post
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
+
+// delete User
+// http.delete('https://jsonplaceholder.typicode.com/users/2')
+// .then(data => console.log(data))
+// .catch(err => console.log(err))
